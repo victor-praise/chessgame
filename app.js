42ca8594dbb6d35ce8e62ba9ca2887f31fe92f90
victor-praise <vpraise27@gmail.com>
2023-07-18 12:18:32 -0400

    chess board added

---
+++ app.js
@@ -0,0 +1,28 @@
+const gameBoard = document.querySelector("#gameboard")
+const playerDisplay = document.querySelector("#player")
+
+const infoDisplay = document.querySelector("#info-display")
+const width = 8
+
+const startPieces = [
+    rook,knight,bishop,queen,king,bishop,knight,rook,
+    pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,
+    '','','','','','','','',
+    '','','','','','','','',
+    '','','','','','','','',
+    '','','','','','','','',
+    pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,
+    rook,knight,bishop,queen,king,bishop,knight,rook
+]
+
+function createBoard(){
+    startPieces.forEach((startPiece,i)=>{
+        const square = document.createElement('div')
+        square.setAttribute('square-id', i)
+        square.classList.add('square')
+        square.classList.add('beige')
+        gameBoard.append(square)
+    })
+}
+
+createBoard()
