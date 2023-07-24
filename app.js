809adc7fe961c44f642b6f3683ede90753160fd7
victor-praise <vpraise27@gmail.com>
2023-07-24 17:53:34 -0400

    pieces added

--- app.js
+++ app.js
@@ -6,23 +6,63 @@ const width = 8
 
 const startPieces = [
     rook,knight,bishop,queen,king,bishop,knight,rook,
-    pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,
+    pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,
     '','','','','','','','',
     '','','','','','','','',
     '','','','','','','','',
     '','','','','','','','',
-    pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,
+    pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,
     rook,knight,bishop,queen,king,bishop,knight,rook
 ]
 
 function createBoard(){
     startPieces.forEach((startPiece,i)=>{
         const square = document.createElement('div')
-        square.setAttribute('square-id', i)
         square.classList.add('square')
-        square.classList.add('beige')
+        square.innerHTML = startPiece
+        square.firstChild?.setAttribute('draggable',true)
+        square.setAttribute('square-id', i)
+        
+        
+        //square.classList.add('beige')
+        const row = Math.floor((63-i)/8) + 1
+
+    if(row%2===0){
+        square.classList.add(i % 2 === 0 ? "beige" : "brown")
+    }
+    else{
+        square.classList.add(i % 2 === 0 ? "brown" : "beige")
+    }
+    if(i<=15){
+        square.firstChild.classList.add('black')
+    }
+
+    if(i>=48){
+        square.firstChild.classList.add('white')
+    }
+
         gameBoard.append(square)
     })
+
+
+  
 }
 
 createBoard()
+
+
+const allSquares = document.querySelectorAll("#gameboard .square")
+allSquares.forEach(square=>{
+    square.addEventListener('dragstart',dragStart)
+    square.addEventListener('dragover',dragOver)
+})
+
+let startPositionId
+let draggedElement
+function dragStart(e){
+startPositionId = e.target.parentNode.getAttribute('square-id')
+draggedElement = e.target
+}
+function dragOver(e){
+e.preventDefault()
+}
